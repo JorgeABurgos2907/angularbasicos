00fe25af9b641c52265c51d7ee44e35c2768c1c8
Jorge Burgos <burgosjorge2907@gmail.com>
2021-11-09 23:40:19 -0500

    Listado Personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent{
 
+  personajes: Personaje [] = [
+    {
+      nombre: 'goku',
+      poder: 9000
+    },
+    {
+      nombre: 'vegeta',
+      poder: 7000
+    },
+    {
+      nombre: 'krilin',
+      poder: 700
+    }
+  ];
+
   agregar(){
-    console.log('Hey!!!');
+
+    if(this.nuevo.nombre.trim().length != 0 && this.nuevo.poder != undefined ){
+      this.personajes.push(this.nuevo);
+      this.nuevo = {
+        nombre: '',
+        poder: 0
+      }
+
+      console.log("Personaje Añadido");
+      // console.log(this.nuevo);
+
+      return;
+    }
+
+    
+  }
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
   }
 
 }
